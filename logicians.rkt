3ef40142dc04dc7b2e93910d1085b00c1b4cb130
Jude <julie_wang1@brown.edu>
2020-04-17 22:18:43 -0400

    we started on the trace and r crying

--- logicians.rkt
+++ logicians.rkt
@@ -24,6 +24,7 @@ one sig Na extends Answer{}
 
 sig State {
     worlds: set Logician->World->World
+    answered: set Logician -- who has answered the Bartender already
 }
 
 one sig InitState extends State {} -- delete later
@@ -35,10 +36,6 @@ pred setup {
         all l : Logician |
             one (w.preferences)[l]
     }
-
-    -- all possible combinations of preferences are in a world
-    --all l: Logician, b: Boolean | divide[# (World), 2] = # ({w: World | l->b in w.preferences})
-
 
     -- attempt 2: no worlds have the same preferences
     all w1: World, w2: World - w1 | not (w1.preferences = w2.preferences)
@@ -61,23 +58,26 @@ pred consistent[l: Logician, w1: World, w2: World] {
     w1.preferences[l] = w2.preferences[l]
 }
 
-pred iState {
-    all l: Logician, w1: World, w2: World | consistent[l, w1, w2] iff l->w1->w2 in InitState.worlds
-}
-
-run { setup iState } for exactly 3 Logician, 1 State, exactly 8 World
+run { setup } for exactly 3 Logician, 1 State, exactly 8 World
 
 state[State] initState {
    all l: Logician, w1: World, w2: World | consistent[l, w1, w2] iff l->w1->w2 in worlds
+   no answered -- no one has answered yet
 }
 
 transition[State] logicianSays[e: Event] {
     e.pre = this
     e.post = this'
+    -- propagate that answer and update worlds
+}
+
+pred validAnswer[e: Event] {
+    -- find a Logician that hasn't answered yet
+    -- make that Logician think and answer
 }
 
 transition[State] step {
-    some e: Event | logicianSays[this, this', e] -- this has to be one Logician answering something
+    some e: Event | validAnswer[e] and logicianSays[this, this', e] -- this has to be one Logician answering something
 }
 
 
@@ -87,6 +87,6 @@ transition[State] step {
     --some Goat & animals implies (#(Goat & animals) >= #(Wolf & animals))
 --}
 
---trace<|State, initState, step, _|> traces: linear {}
+trace<|State, initState, step, _|> traces: linear {}
 
---run<|traces|> neverEating for exactly 12 State, 11 Event, 6 Animal, exactly 3 Goat, exactly 3 Wolf, 2 Position, 4 Int
+run<|traces|> for exactly 12 State, 11 Event, 6 Animal, exactly 3 Goat, exactly 3 Wolf, 2 Position, 4 Int
