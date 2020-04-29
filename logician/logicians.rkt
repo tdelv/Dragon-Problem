385eb2a6b687312ea5ab5a02d5f26f0b43767f65
Jude <julie_wang1@brown.edu>
2020-04-29 17:03:38 -0400

    set up with evidence graphs

--- logician/logicians.rkt
+++ logician/logicians.rkt
@@ -5,7 +5,6 @@ sig Logician {}
 sig World {
     preferences: set Logician->Boolean
 }
-one sig TrueWorld extends World {} --defined to be TTT in setup predicate
 
 sig Boolean {} -- symbolizes if a logician wants a drink or not
 one sig True extends Boolean {}
@@ -17,7 +16,8 @@ one sig Ya extends Answer{}
 one sig Na extends Answer{}
 
 sig State {
-    worlds: set Logician->World->World,
+    knowledge: set Logician->World->World,
+    evidence: set Logician->Answer->World,
     answered: set Logician -- who has answered the bartender already
 }
 
@@ -32,72 +32,55 @@ pred setup {
     -- all unique worlds are in the set (specific number constrained in run statement)
     all w1: World, w2: World - w1 | not (w1.preferences = w2.preferences)
 
-    -- defining the Trueworld to be where all logicians want drink
-    all l: Logician | l->True in TrueWorld.preferences
-
     -- well formed stuff
     Boolean = True + False
     Answer = Idk + Ya + Na
-    one TrueWorld
 }
 
 
 sig Event {
     speaker: one Logician,
-    answer: one Answer,
     pre: one State,
     post: one State
 }
-
 
 -- constrains all worlds that connect to each other
 pred consistent[l: Logician, w1: World, w2: World] {
     w1.preferences[l] = w2.preferences[l]
 }
 
+pred consistentEvidence[w: World, knowledge: World->World, a: Answer] {
+    (all connected: knowledge[w] | connected.preferences[Logician] = True) => a = Ya
+    else {
+        (all connected: knowledge[w] | False in connected.preferences[Logician]) => a = Na
+        else a = Idk
+    }
+}
+
 --run { setup } for exactly 3 Logician, 1 State, exactly 8 World
 
 state[State] initState {
-   all l: Logician, w1: World, w2: World | consistent[l, w1, w2] iff l->w1->w2 in worlds
+   all l: Logician, w1: World, w2: World | consistent[l, w1, w2] iff l->w1->w2 in knowledge
+   all l: Logician, w: World, a: Answer | consistentEvidence[w, knowledge[l], a] iff l->a->w in evidence
    no answered -- no one has answered yet
 }
 
 transition[State] logicianSays[e: Event] {
     answered' = answered + e.speaker
     
-    -- propagate that answer and update worlds
+    -- build evidence graph for this speaker
+    -- intersect everyone else's knowledge graph with that evidence graph
     all l: Logician | {
-        -- this feels like cheating lol
-        -- are we supposed to break it down into cases where if they answer yes then we do this and if they ansewr no then we do that etc
-        all w1: World, w2: World | (l->w1->w2 in worlds and consistent[e.speaker, w1, w2]) iff l->w1->w2 in worlds'
+        all w1: World, w2: World | (l->w1->w2 in knowledge and consistent[e.speaker, w1, w2]) iff l->w1->w2 in knowledge'
     }
-    
-    -- if logicianX says no -- we want all worlds where logicianX wants drink to have no edges to worlds where logicianX doesn't want drink,
-                            -- these worlds shouldn't connect to TrueWorld anymore
-    -- if logicianX says yes -- true world should have no edges coming out of it
-    -- if logicianX says idk -- all worlds where logicianX wants drink should maintain their connections to TrueWorld, everything else
-                             -- can be thrown away
 
     e.pre = this
     e.post = this'
 }
 
--- constrains a logician to answer logically
-pred validAnswer[e: Event] {
-    -- find a Logician that hasn't answered yet
-    e.speaker not in e.pre.answered
-    
-    -- make that Logician think and answer logically
-    (all w: e.pre.worlds[e.speaker][TrueWorld] | w.preferences[Logician] = True) => e.answer = Ya
-    else {
-        (all w: e.pre.worlds[e.speaker][TrueWorld] | False in w.preferences[Logician] ) => e.answer = Na
-        else e.answer = Idk
-    }
-}
-
 -- wrapper for an event to happen
 transition[State] step {
-    some e: Event | validAnswer[e] and logicianSays[this, this', e]
+    some e: Event | logicianSays[this, this', e]
 }
 
 trace<|State, initState, step, _|> traces: linear {}
